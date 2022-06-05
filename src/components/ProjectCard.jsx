3647b4fc1b38343d6c8579209d3b2ee6f506440b
abdulqadir <cajiibxaaji02@gmail.com>
2022-06-05 07:05:00 +0300

    project

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import {AiFillGithub } from 'react-icons/ai' 
 import {BiLinkExternal } from 'react-icons/bi' 
 
-const ProjectCard = () => {
+const ProjectCard = ({title, info, tags}) => {
   return (
     <div>
             {/* card */}
-            <div className='bg-gray-800 rounded p-4 w-80'>
+            <div className='bg-gray-800 rounded p-4 w-80 h-48'>
                 {/* title and icon */}
                 <div className='flex justify-between'>
-                    <h1 className='text-xl flex-1'>Food Menu</h1>
+                    <h1 className='text-xl flex-1'>{title}</h1>
                     <div className='flex gap-4 my-2'>
                         <a href="#"><AiFillGithub size={20} /></a>
                         <a href="#"><BiLinkExternal size={20} /></a>
@@ -17,12 +17,11 @@ const ProjectCard = () => {
                 </div>
                 {/* info */}
                 <div className='my-4 text-ellipsis overflow-hidden'>
-                    <p>An application to search for different recipes built using the Edamam API.</p>
+                    <p>{info}</p>
                 </div>
                 {/* tags */}
                 <div className='flex gap-2'>
-                    <span className='bg-[#481261] text-sm px-2 py-1'>React js</span>
-                    <span className='bg-[#481261] text-sm px-2 py-1'>Tailwindcss</span>
+                    <span className='bg-[#481261] text-sm px-2 py-1'>{tags}</span>
                 </div>
             </div>
     </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProjectCard from './ProjectCard'
 
 const Projects = () => {
+    const [projects, setProjects] = useState([
+        {
+            id :1,
+            title: 'ShipQuik Logistics', 
+            info: 'A proposed website for a logistics company based in Lagos.',
+            tags: ['Reactjs', 'Tailwind CSS']
+        },
+        {
+            id :2,
+            title: 'Task Manager', 
+            info: 'A task manager app built with React'},
+   
+    ])
   return (
     <div className='mt-8'>
        <h1 className='text-2xl font-medium'>Projects</h1>
        {/* cards */}
        <div className='mt-8 md:grid grid-cols-3 md:gap-4'>
-           <ProjectCard/>
-           <ProjectCard/>
-           <ProjectCard/>
-           <ProjectCard/>
+           {projects.map((project)=>(
+               <ProjectCard key={project.id} title={project.title} info={project.info} tags={project.tags}/>
+           ))}
        </div>
     </div>
   )
