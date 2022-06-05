51401d295952f0b1958e663dc5663c90bafe3fa1
abdulqadir <cajiibxaaji02@gmail.com>
2022-06-05 22:35:20 +0300

    firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+
 import './App.css';
 import Home from './components/Home';
 
+
 function App() {
+
   return (
-    <div className='bg-gray-900 text-white w-full h-full font-[poppins]'>
+    <div className='bg-gray-900 text-white w-full h-screen font-[poppins]'>
       <Home/>
+ 
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
 import Profile from './Profile'
 import Projects from './Projects'
+import { db } from "../firebase";
+import {getDocs, collection} from '../firebase/firestore'
 
 const Home = () => {
+  const [users, setUsers] = useState([])
+  const usersCollectionRef = collection(db, "projects");
+
+  useEffect(()=>{
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+    };
+getUsers();
+  },[])
+
+
   return (
     <div className='w-[80%] mx-auto'>
         <Profile/>
